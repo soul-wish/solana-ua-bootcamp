e41571e24afa1e931047a537a54a74982bd0c49e
Sergii Lysenko <soulwish.ls@gmail.com>
2025-04-15 14:20:40 -0400

    add test, create simple program

---
+++ practice-3/favorites/programs/favorites/src/lib.rs
@@ -0,0 +1,47 @@
+use anchor_lang::prelude::*;
+
+declare_id!("t8uNTr7uH8D283hwuuHBbA4kFU8RcLfR3QLPaDouDMV");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+}
+
+#[derive(Accounts)]
+pub struct AddFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[program]
+pub mod favorites {
+    use super::*;
+
+    pub fn set_favorites(ctx: Context<AddFavorites>, number: u64, color: String) -> Result<()> {
+        let user_public_key = ctx.accounts.user.key();
+        msg!("Greeting from: {}", ctx.program_id);
+        msg!("User {}'s favorite number is {} and favorite color is {}", user_public_key, number, color);
+        ctx.accounts.favorites.set_inner(Favorites { number, color });
+        Ok(())
+    }
+}
---
+++ practice-3/favorites/tests/favorites.test.ts
@@ -0,0 +1,59 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program, web3 } from "@coral-xyz/anchor";
+import { Favorites } from "../target/types/favorites";
+import {
+  airdropIfRequired,
+  getCustomErrorMessage,
+} from "@solana-developers/helpers";
+import { expect, describe, test } from "@jest/globals";
+import { systemProgramErrors } from "./system-program-errors";
+
+describe("favorites", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  it("Writes our favotites to the blockchain", async () => {
+    const user = web3.Keypair.generate();
+    const program = anchor.workspace.Favorites as Program<Favorites>;
+
+    console.log("Our user address: ", user.publicKey.toBase58());
+
+    await airdropIfRequired(
+      anchor.getProvider().connection,
+      user.publicKey,
+      0.5 * web3.LAMPORTS_PER_SOL,
+      1 * web3.LAMPORTS_PER_SOL,
+    );
+
+    const favoriteNumber = new anchor.BN(42);
+    const favoriteColor = "Orange";
+
+    let tx: string | null = null;
+    try {
+      tx = await program.methods
+        .setFavorites(favoriteNumber, favoriteColor)
+        .accounts({
+          user: user.publicKey,
+        })
+        .signers([user])
+        .rpc();
+    } catch (e) {
+      const rawError = e as Error;
+      throw new Error(
+        getCustomErrorMessage(systemProgramErrors, rawError.message),
+      );
+    }
+
+    console.log("Transaction hash: ", tx);
+
+    const [favoritesPDA, _bump] = web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("favorites"), user.publicKey.toBuffer()],
+      program.programId,
+    );
+
+    const dataFromPDA = await program.account.favorites.fetch(favoritesPDA);
+
+    expect(dataFromPDA.number.toNumber()).toEqual(favoriteNumber.toNumber());
+    expect(dataFromPDA.color).toEqual(favoriteColor);
+  });
+});
